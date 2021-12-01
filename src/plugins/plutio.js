571f2611051db082f0d672a0132a341a1ce0e2e1
David Höglinger <david.hoeglinger@gmail.com>
2021-12-01 15:16:19 +0100

    Added Crisp chat

--- src/plugins/plutio.js
+++ src/plugins/plutio.js
@@ -1,12 +1,22 @@
-(function (p, l, u, t, i, o) {
-  p[t] = p[t] || function () {
-    (p[t].q = p[t].q || []).push(arguments);
-  };
-  o = l.getElementsByTagName('head')[0];
-  i = l.createElement('script');
-  i.async = 1;
-  i.src = u;
-  o.appendChild(i);
-})(window, document, 'https://cdn.plutio.com/messenger/main.js', '$plutio_msg');
+// (function(p, l, u, t, i, o) {
+//   p[t] = p[t] || function() {
+//     (p[t].q = p[t].q || []).push(arguments);
+//   };
+//   o = l.getElementsByTagName("head")[0];
+//   i = l.createElement("script");
+//   i.async = 1;
+//   i.src = u;
+//   o.appendChild(i);
+// })(window, document, "https://cdn.plutio.com/messenger/main.js", "$plutio_msg");
+//
+// $plutio_msg("WkJJSuArLLBAchu6Z", {});
 
-$plutio_msg('WkJJSuArLLBAchu6Z', { });
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "355a20db-23cd-4cae-9f98-7df72ce486b9";
+(function() {
+  d = document;
+  s = d.createElement("script");
+  s.src = "https://client.crisp.chat/l.js";
+  s.async = 1;
+  d.getElementsByTagName("head")[0].appendChild(s);
+})();
